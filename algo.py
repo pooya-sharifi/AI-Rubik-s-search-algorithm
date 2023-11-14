9bf0f56c39eb4428049c63f288a24dd042cb4314
pooya sharifi <sharifipooya@gmail.com>
2023-11-14 11:53:53 +0330

    Done

--- algo.py
+++ algo.py
@@ -2,7 +2,7 @@ import numpy as np
 from state import next_state, solved_state
 from location import next_location
 from collections import OrderedDict
-
+import heapq
 
 
 def solve(init_state, init_location, method):
@@ -28,6 +28,45 @@ def solve(init_state, init_location, method):
         cost = 0
         parent = None
         move = None
+
+    class NodeA:
+        # def __init__(self):
+        cube = None
+        cost = 0
+        parent = None
+        move = None
+        heuristic=None
+        location=None
+        def __lt__(self, other):
+        # Compare based on the sum of cost and heuristic
+            return (self.cost + self.heuristic) < (other.cost + other.heuristic)
+
+
+    def heuristic(current_position):
+        goal_cube = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+        #y of first line is 0
+        #x of left is zero
+        # z is back I guess
+        distance = 0
+        current_position=np.array(current_position)
+        for z in range(2):
+            for col in range(2):
+                for row in range(2):
+                    if(goal_cube[row,col,z]==current_position[row,col,z]):
+                        print("success",row,col,z)
+
+                    else:
+                        # print("goal",goal_cube[row,col,z])
+                        for z_2 in range(2):
+                            for col_2 in range(2):
+                                for row_2 in range(2):
+                                    if goal_cube[row,col,z]==  current_position[row_2,col_2,z_2]:
+                                        # print("scooby doo by doo where are you",row_2,col_2,z_2)
+                                        # print("should have been at", row,col,z)
+                                        distance=distance+np.abs(row_2-row)+np.abs(col_2-col)+np.abs(z_2-z)
+
+                    # distance += abs(goal_cube[0] - current_position[0]) + abs(goal_cube[1] - current_position[1]) + abs(goal_cube[2] - current_position[2])
+        return distance/4
 
     if method == 'Random':
         return list(np.random.randint(1, 12+1, 10))
@@ -122,7 +161,70 @@ def solve(init_state, init_location, method):
         return list(np.random.randint(1, 12+1, 10))
     
     elif method == 'A*':
-        ...
+        # curr_cube = [[[2, 6], [3, 4]], [[1, 5], [7, 8]]]
+        # dist=manhattan_distance(curr_cube)
+        # print("heuristic",dist)
+        cost_limit = 1
+        nodes = 0
+        moves = list()
+        frontier = []
+        heapq.heapify(frontier)  # Priority queue using heapq
+        solved = solved_state()
+
+        print(init_state)
+        print("A*")
+
+        start = NodeA()
+        start.cube = init_state
+        start.cost = 0
+        start.location=init_location
+        start.heuristic = heuristic(init_location)  # Calculate heuristic for start node
+        heapq.heappush(frontier, (start.cost + start.heuristic, start))
+        print("before we start",frontier[0][1].cube)
+        visited = set()
+        
+        while frontier:
+            print("size",np.size(frontier))
+            _, curr = heapq.heappop(frontier)
+
+            if hash(curr.cube.tobytes()) in visited:
+                continue
+
+            visited.add(hash(curr.cube.tobytes()))
+            if hash(curr.cube.tobytes()) == hash(solved.tobytes()):
+                # Goal state reached
+                # print("reached goal")
+                # while curr.parent is not None:
+                #     moves.append(curr.move)
+                #     curr = curr.parent
+                # moves.reverse()
+                # print("moves",moves)
+                # return moves
+                node_search=curr
+                while(node_search.move != None):
+                    print("while success move",node_search.move)
+                    moves.append(node_search.move)
+                    node_search=node_search.parent
+                moves.reverse()
+                print("moves",moves)
+                return moves
+
+            if curr.cost + 1 <= cost_limit:
+                child_cost = curr.cost + 1
+                for i in range(12):
+                    nodes += 1
+                    new_node = NodeA()
+                    new_node.cube = next_state(curr.cube, i + 1)
+                    new_node.cost = child_cost
+                    new_node.location= next_location(curr.location,i+1)
+                    new_node.heuristic = heuristic(new_node.location)
+                    new_node.parent = curr
+                    new_node.move = i + 1
+                    print("what we are checking",new_node.cost + new_node.heuristic)
+                    heapq.heappush(frontier, (new_node.cost + new_node.heuristic, new_node))
+
+            cost_limit += 1
+
 
     elif method == 'BiBFS':
         cost_limit = 1
